6f6978ea3b4cf90c6aed842bd8a1e7f60e72b2e3
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-12-03 14:38:43 +0100

    added comment model

---
+++ Halo/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Halo.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        /*public int Upvotes { get; set; }
+        public int Downvotes { get; set; }  */
+        public IdentityUser RegisteredUser { get; set; }
+        public List<Report> Reports { get; set; }   
+
+        public Comment()
+        {
+            
+        }
+    }
+}
