83c2acf94713ee0c7e30c2e15af3918da2973fcc
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-12-03 14:39:40 +0100

    added user abstract class

---
+++ Halo/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Halo.Models
+{
+    public abstract class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+    }
+}
