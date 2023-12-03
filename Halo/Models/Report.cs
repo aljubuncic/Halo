356a269c9cbd63d69ca97b2c2765ff73d579a0d0
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-12-03 14:39:30 +0100

    added report model

---
+++ Halo/Models/Report.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Halo.Models
+{
+    public class Report
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        public Comment Comment { get; set; }
+        public RegisteredUser RegisteredUser { get; set; }
+        public string Reason { get; set; }
+        public Report()
+        {
+            
+        }
+    }
+}
