c6ff4c21e117c5565aa34183fa8b631091888665
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-12-03 14:38:04 +0100

    added avdertisement model

---
+++ Halo/Models/Advertisement.cs
@@ -0,0 +1,14 @@
+﻿namespace Halo.Models
+{
+    public class Advertisement
+    {
+        public int Id { get; set; }
+        public string AdvertiserEmail { get; set; }
+        public string Image { get; set; } //treba skontat kako evidentirat tipove oglasavanja, ovdje sam samo stavio sliku ako se radi o banneru
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int NumberOfDays { get; set; }
+        public double Price { get; set; }   
+        public Advertisement() { }
+    }
+}
