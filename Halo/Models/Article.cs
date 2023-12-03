4223baec5b9ce17f8ec1401811b78656de72a2e0
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-12-03 14:38:14 +0100

    added article model

---
+++ Halo/Models/Article.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Halo.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public ArticleCategory Category { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        public Journalist Journalist { get; set; }
+        public string Content { get; set; }
+        public List<string> Images { get; set; }
+        //mozda? public List<string> Tags { get; set; }
+        public List<Comment> Comments { get; set; }
+        public List<ArticleMistake> ReportedMistakes { get; set; }
+        public Article()
+        {
+            
+        }
+
+    }
+}
