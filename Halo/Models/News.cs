b76511e728dc189fdaa9beef291678a68a5d07f4
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-12-03 14:39:11 +0100

    added news model

---
+++ Halo/Models/News.cs
@@ -0,0 +1,19 @@
+﻿namespace Halo.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public List<string> Images { get; set; }
+        //ovdje sam mislio da ne ubacujem objekat usera
+        //jer ipak trebaju formalnije informacije kojih nema u tom objektu
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public News()
+        {
+            
+        }
+    }
+}
