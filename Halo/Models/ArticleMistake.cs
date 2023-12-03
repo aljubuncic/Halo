ece5d5e87f301af3f93623a73a1e5332064b2d5b
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-12-03 14:38:33 +0100

    added articlemistake model

---
+++ Halo/Models/ArticleMistake.cs
@@ -0,0 +1,9 @@
+﻿namespace Halo.Models
+{
+    public class ArticleMistake
+    {
+        public int Id { get; set; }
+        public Article Article { get; set; }
+        public string MistakeExplanation { get; set; }
+    }
+}
