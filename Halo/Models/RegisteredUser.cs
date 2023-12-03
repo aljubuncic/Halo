eeed5ac7c126644fafc034e23403d790e6c76e20
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-12-03 14:39:22 +0100

    added registereduser model

---
+++ Halo/Models/RegisteredUser.cs
@@ -0,0 +1,9 @@
+﻿namespace Halo.Models
+{
+    public class RegisteredUser : User
+    {
+        public List<Comment> Comments { get; set; } 
+        public List<Report> Reports { get; set; }
+        public RegisteredUser() { }
+    }
+}
